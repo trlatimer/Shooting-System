d6d5659383c7404c95600acf7311c733389a9fc6
trlatimer <trlatimer95@gmail.com>
2020-10-07 21:19:28 -0700

    Created script for handling weapon mechanics Implemented basic fire and aiming behavior

--- Shooting System/Assets/Scripts/Weapon.cs
+++ Shooting System/Assets/Scripts/Weapon.cs
@@ -1,10 +1,20 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
     [SerializeField] Camera FPCamera;
+    [Tooltip("Represents rounds per minute (e.g. 60 would mean you can shoot once every second)")] 
+    [SerializeField] float fireRate = 20f;
+
+    bool canShoot = true;
+
+    private void OnEnable()
+    {
+        canShoot = true;
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -15,6 +25,28 @@ public class Weapon : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0) && canShoot)
+        {
+            StartCoroutine(Shoot());
+        } 
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            GetComponent<Animator>().SetBool("isAiming", true);
+        } else if (Input.GetMouseButtonUp(1))
+        {
+            GetComponent<Animator>().SetBool("isAiming", false);
+        }
+    }
+
+    private IEnumerator Shoot()
+    {
+        canShoot = false;
+        Debug.Log("Shot!");
+        // play audio
+        // play muzzle flash
+        // process parabolic raycast with gravity and wind drop
+        yield return new WaitForSeconds(60 / fireRate);
+        canShoot = true;
     }
 }
